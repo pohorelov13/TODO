563697a37975486322a86e80f5806c7838d76e1c
Your Name <you@example.com>
2024-03-18 14:13:42 +0200

    Homework done

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+const list = document.createElement('ul');
+
 function getToDoList() {
     const toDoList = [];
     let toDo;
-    while (toDo = prompt('ENTER TODO')) {
+    while (toDo = getTask()) {
         toDoList.push(toDo);
     }
     return toDoList;
@@ -9,18 +11,53 @@ function getToDoList() {
 
 function createList(toDoList) {
     if (Array.isArray(toDoList)) {
-        const list = document.createElement('ul');
+        list.id = 'todo-list'
         toDoList.forEach(e => {
             const listElement = document.createElement('li');
             listElement.textContent = e;
+            addCloseX(listElement, list);
             list.append(listElement);
         });
         document.body.append(list);
     }
 }
 
+function addCloseX(liElement) {
+    const span = document.createElement("SPAN");
+    const txt = document.createTextNode("X");
+    span.className = "close";
+    span.appendChild(txt);
+    span.onclick = function () {
+        this.parentElement.remove();
+    }
+    liElement.appendChild(span);
+}
+
+function getTask() {
+    return prompt('ENTER TODO');
+}
+
+function addNewTask() {
+    const task = getTask();
+    if (task !== null) {
+        const liElement = document.createElement('li');
+        liElement.textContent = task;
+        addCloseX(liElement);
+        list.append(liElement);
+    }
+}
+
+function createAddButton() {
+    const htmlButtonElement = document.createElement('button');
+    htmlButtonElement.textContent = '+';
+    htmlButtonElement.className = 'add-button'
+    htmlButtonElement.addEventListener('click', () => addNewTask());
+    document.body.append(htmlButtonElement);
+}
+
 function init() {
     createList(getToDoList());
+    createAddButton();
 }
 
 init();
